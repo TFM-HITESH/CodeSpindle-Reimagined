520bbb1eb2d0a70bedf2b15e46b61a046c315c50
TFM-HITESH <hiteshshivkumar@gmail.com>
2023-08-21 01:14:07 +0530

    Navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 type NavbarProps = {
-    
+
 };
 
-const Navbar:React.FC<NavbarProps> = () => {
-    
-    return <div>Have a good coding</div>
+const Navbar: React.FC<NavbarProps> = () => {
+
+    return (
+        <div>
+            Have a good coding
+        </div>
+    )
 }
 export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Navbar from '@/Components/Navbar'
 import Image from 'next/image'
 
 export default function Home() {
   return (
+    
     <div>
+      <Navbar />
       Hello world !
     </div>
   )
